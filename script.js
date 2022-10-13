7254d479313d36446f49db8e5ae7062013b8d7ee
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-10-12 22:05:21 -0600

    Add Operations Added Add,Subtract,Multiply and Divide Operations Added Operate function, Takes operation and 2 values Fixed typo where script was not a javascript file Added BoilerPlate to index.html

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(a,b){
+    console.log(a+b)
+    return a+b;
+}
+
+function subtract(a,b){
+     console.log(a-b)
+    return a-b;
+}
+
+function multiply(a,b){
+     console.log(a*b)
+    return a*b;
+}
+
+function divide(a,b){
+    console.log(a/b)
+    return a/b;
+}
+
+function operate(operator,a,b){
+    switch (operator){
+        case '+':
+            add(a,b);
+            break;
+        case '-':
+            subtract(a,b);
+            break;
+        case '*':
+            multiply(a,b);
+            break;
+        case '/':
+            divide(a,b);
+            break;
+        default:
+            console.log('ERROR!')
+
+    }
+}
+
