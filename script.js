b938e713a8046a652867f257bcd34c00f55b46c3
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2022-10-13 21:02:03 -0600

    Add Display Functions Added function to change Display Added event listeners to buttons Changed display default value to 0

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-function add(a,b){
-    console.log(a+b)
-    return a+b;
+let btns = document.querySelectorAll('button');
+let display=document.querySelector('.calculator-display');
+let computation=''
+
+
+function add(a, b) {
+    console.log(a + b)
+    return a + b;
 }
 
-function subtract(a,b){
-     console.log(a-b)
-    return a-b;
+function subtract(a, b) {
+    console.log(a - b)
+    return a - b;
 }
 
-function multiply(a,b){
-     console.log(a*b)
-    return a*b;
+function multiply(a, b) {
+    console.log(a * b)
+    return a * b;
 }
 
-function divide(a,b){
-    console.log(a/b)
-    return a/b;
+function divide(a, b) {
+    console.log(a / b)
+    return a / b;
 }
 
-function operate(operator,a,b){
-    switch (operator){
+function operate(operator, a, b) {
+    switch (operator) {
         case '+':
-            add(a,b);
+            add(a, b);
             break;
         case '-':
-            subtract(a,b);
+            subtract(a, b);
             break;
         case '*':
-            multiply(a,b);
+            multiply(a, b);
             break;
         case '/':
-            divide(a,b);
+            divide(a, b);
             break;
         default:
             console.log('ERROR!')
@@ -38,3 +43,17 @@ function operate(operator,a,b){
     }
 }
 
+function updateDisplay(string){
+    display.innerText=string;
+}
+
+function updateComputation(char){
+    console.log(char);
+    computation+=char;
+    updateDisplay(computation)
+}
+
+
+for (let i of btns) {
+    i.addEventListener('click', ()=>updateComputation(i.innerText)) ;
+}
